3bcdfc33a3a456a878b9f8638d3aa8ba807acd5d
Sadikur Rahman Sajib <53177822+sadikurtechnobd@users.noreply.github.com>
2019-12-22 23:22:44 +0600

    some file added

---
+++ send.php
@@ -0,0 +1,18 @@
+<?php
+
+$to = "sadikurrahman5295@gmail.com";
+
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$sub = "codersadikur.com";
+$message = $_REQUEST['message'];
+
+
+$msg = "Name : ".$name."\n";
+$msg .= "Email : ".$email."\n";
+$msg .= "Message :".$message."\n";   
+   
+$mail = mail($to,$sub,$msg);
+header('location:index.html');
+
+?>
